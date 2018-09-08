ded38d39e9bc38002547879c01ed957c5fc8912b
organisation2019 <skumar2019@outlook.com>
2018-09-08 04:12:27 +0300

    Codility Java Test Answers

--- src/com/codility/assignment/TapeEquilibriumTest.java
+++ src/com/codility/assignment/TapeEquilibriumTest.java
@@ -2,27 +2,73 @@ package com.codility.assignment;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class TapeEquilibriumTest {
 
 	@Test
 	public void test() {
-		assertEquals(4, solution(new int[] {2,3,1,5}));
+		assertEquals(1, solution(new int[] { 3, 1, 2, 4, 3 }));
 	}
-	
-	
-	public int solution(int[] A) {
-		int n = A.length+1;
-		int sum = (n*(n+1))/2;
-		int arraySum = 0; 
-		int i=0;
-		while(i<A.length) {
-			arraySum+=A[i];
-			i++;
+
+	//O(N^2) - Feasible Solution
+	public int solution1(int[] A) {
+		int n = A.length;
+		int diff[] = new int[n - 1];
+		for (int p = 1; p < A.length; p++) {
+			diff[p - 1] = Math.abs(sum(0, p - 1, A) - sum(p, n - 1, A));
 		}
-		return sum - arraySum;
+		Arrays.sort(diff);
+		return diff[0];
+	}
+
+	//O(N^2) - Feasible Solution
+	public int solution2(int[] A) {
+		int n = A.length;
+		int diff[] = new int[n - 1];
+		int firstPartSum = 0;
+		int secondPartSum = 0;
+		for (int p = 1; p < A.length; p++) {
+
+			firstPartSum += A[p - 1];
+			secondPartSum = sum(p, n - 1, A);
+			diff[p - 1] = Math.abs(firstPartSum - secondPartSum);
+		}
+		Arrays.sort(diff);
+		return diff[0];
+	}
+
+	//O(N) - Optimal Solution
+	public int solution(int[] A) {
+		int n = A.length;
+		int diffMin = 0;
+		int diffMax = 0;
+		int sum = sum(0,n-1,A);
+		int firstPartSum = 0;
+		int secondPartSum = 0;
+		for (int p = 0; p < A.length - 1; p++) {
+			firstPartSum += A[p];
+			secondPartSum = sum - firstPartSum;
+			diffMax = Math.abs(firstPartSum - secondPartSum);
+			if (p != 0) {
+				if (diffMax < diffMin) {
+					diffMin = diffMax;
+				}
+			} else {
+				diffMin = diffMax;
+			}
+		}
+		return diffMin;
+	}
+
+	public int sum(int start, int end, int[] A) {
+		int sum = 0;
+		for (int i = start; i <= end; i++) {
+			sum += A[i];
+		}
+		return sum;
 	}
 
 }
-
